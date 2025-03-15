6512f5b038a94c5326f2bd2eef9d678ec54d7faa
Alohadancemeow <nawongsak@gmail.com>
2025-03-15 17:54:54 +0700

    generateObject

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,8 +1,8 @@
 import { getQuestionsSchema } from "@/schemas/questions";
 import { NextResponse } from "next/server";
-import { ZodError } from "zod";
+import { ZodError, z } from "zod";
 
-import { generateText } from "ai";
+import { generateObject } from "ai";
 import { google } from "@ai-sdk/google";
 
 export async function POST(req: Request) {
@@ -22,36 +22,30 @@ export async function POST(req: Request) {
 
     // Structured prompt to enforce JSON output
     const prompt = `
-      Generate ${amount} multiple-choice questions about ${topic}.
-      Return the response in a JSON array format where each object follows this structure:
-        {
-          "question": "question text",
-          "answer": "correct answer (max 15 words)",
-          "option1": "wrong option 1 (max 15 words)",
-          "option2": "wrong option 2 (max 15 words)",
-          "option3": "wrong option 3 (max 15 words)"
-        }
-      Ensure all fields are within the word limits.
-`;
+          Generate ${amount} multiple-choice questions about ${topic}.
+          Return the response in a JSON array format where each object follows this structure.
+          Ensure all fields are within the word limits.
+    `;
 
-    const { text } = await generateText({
+    const { object } = await generateObject({
       model: google("models/gemini-2.0-flash-exp"),
-      prompt: prompt,
+      schema: z.object({
+        questions: z.array(
+          z.object({
+            question: z.string().describe("question text"),
+            answer: z.string().describe("correct answer (max 15 words)"),
+            option1: z.string().describe("wrong option 1 (max 15 words)"),
+            option2: z.string().describe("wrong option 2 (max 15 words)"),
+            option3: z.string().describe("wrong option 3 (max 15 words)"),
+          })
+        ),
+      }),
+      prompt,
     });
 
-    // Remove Markdown code block formatting (if present)
-    const finalText = text.replace(/```json|```/g, "").trim();
-    console.log("Final Text:", finalText);
+    // console.log(object, "object");
 
-    // Parse Gemini response to JSON
-    let questions;
-    try {
-      questions = JSON.parse(finalText);
-    } catch (error) {
-      console.error("Error parsing Gemini response:", error);
-    }
-
-    return NextResponse.json({ questions }, { status: 200 });
+    return NextResponse.json({ questions: object.questions }, { status: 200 });
   } catch (error: any) {
     if (error instanceof ZodError) {
       return NextResponse.json(
